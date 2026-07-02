169ca87f169ea4bcf80601c7865758c3116957d4
kms <mindino03@gmail.com>
2026-07-02 18:53:31 +0900

    [NON-56][feat] 메인 레이아웃 인라인 script 콘솔 에러 수정 (테마 FOUC 스크립트)
    https://linear.app/oouya/issue/NON-56/메인-레이아웃-인라인-script-콘솔-에러-수정-테마-fouc-스크립트

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/header";
@@ -42,7 +43,9 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+        <Script id="theme-init" strategy="beforeInteractive">
+          {themeScript}
+        </Script>
         <I18nProvider locale={locale}>
           <Header />
           <SanctionBanner />
